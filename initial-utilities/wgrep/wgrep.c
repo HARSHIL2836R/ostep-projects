dce0767b2d75c89bcd79241989b333229216e03c
HARSHIL2836R <harshilsolanki1912@gmail.com>
2025-01-19 15:33:11 +0530

    implement wgrep

--- initial-utilities/wgrep/wgrep.c
+++ initial-utilities/wgrep/wgrep.c
@@ -3,21 +3,34 @@
 #include <string.h>
 
 int main(int argc, char *argv[]){
-    for(int i = 0; argv[i] ;i++){
-        if (i == 0) continue;
-        FILE *fp = fopen(argv[i],"r");
+    char *searchterm;
+    FILE *fp;
+    if (argc == 1) {
+        printf("wgrep: searchterm [file ...]\n");
+        exit(1);
+    }
+    else if (argc == 2){
+        searchterm = argv[1];
+        fp = stdin;
+    }
+    else {
+        searchterm = argv[1];
+        fp = fopen(argv[2],"r");
         // printf("\n-----%s-----\n\n",argv[i]);
         // printf("Hi %s\n",argv[i]);
         if (fp == NULL){
-            printf("wcat: cannot open file\n");
+            printf("wgrep: cannot open file\n");
             exit(1);
         }
-        char buff[100];
-        while (fgets(buff,sizeof(buff),fp)){ //fget can lead to buffer overflow
-            printf("%s",buff);
-        }
-        // printf("\n");
-        fclose(fp);
     }
+    char* line;
+    __ssize_t nread;
+    size_t len = 0;
+    while ((nread = getline(&line,&len,fp) != -1)){ //fget can lead to buffer overflow
+        // Search for word in line
+        if (strstr(line,searchterm)) printf("%s",line);
+    }
+    //printf("\n");
+    fclose(fp);
     exit(0);
 }
