cfec6766b056e89593d135fec7229400383190c7
HARSHIL2836R <harshilsolanki1912@gmail.com>
2025-01-19 12:35:48 +0530

    Worked out wcat

---
+++ initial-utilities/wcat/wcat.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[]){
+    for(int i = 0; argv[i] ;i++){
+        if (i == 0) continue;
+        FILE *fp = fopen(argv[i],"r");
+        // printf("\n-----%s-----\n\n",argv[i]);
+        // printf("Hi %s\n",argv[i]);
+        if (fp == NULL){
+            printf("wcat: cannot open file\n");
+            exit(1);
+        }
+        char buff[100];
+        while (fgets(buff,sizeof(buff),fp)){
+            printf("%s",buff);
+        }
+        // printf("\n");
+        fclose(fp);
+    }
+    exit(0);
+}
